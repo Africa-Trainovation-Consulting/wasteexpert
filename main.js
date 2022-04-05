196866a6a9349c104977b89ffc8d0da367438920
olaniyiOlawale <jao.olaniyi@gmail.com>
2022-04-05 18:02:32 +0100

    index2Started

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ document.getElementById('hamburger').onclick = function () {
     document.getElementById("hamburger").classList.toggle("fa-times")
     // document.getElementById('nav').classList.toggle("show")
 }
+
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 500,
+  wrap: false
+})
