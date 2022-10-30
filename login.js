0b6b0cfa16cd6cb8e2ae044793326ed87b3263fa
gift56 <efeasiughu@gmail.com>
2022-10-30 20:59:35 +0100

    working on some stuff

--- login.js
+++ login.js
@@ -2,8 +2,10 @@ const form = document.getElementById("loginForm");
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  
+
   const prePayload = new FormData(form);
+
+  console.log([...prePayload]);
 };
 
 form.addEventListener("submit", handleSubmit);
