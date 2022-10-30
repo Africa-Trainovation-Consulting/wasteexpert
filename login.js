6eb116ab5acc2ba64560b0d56303565382afd6ab
gift56 <efeasiughu@gmail.com>
2022-10-30 21:04:33 +0100

    working on some stuff

--- login.js
+++ login.js
@@ -3,9 +3,14 @@ const form = document.getElementById("loginForm");
 const handleSubmit = (e) => {
   e.preventDefault();
 
-  const prePayload = new FormData(form);
+  const payload = new FormData(form);
 
-  console.log([...prePayload]);
+  console.log([...payload]);
+
+  fetch("https://waste-expert-auth.herokuapp.com/api/v1/auth/login", {
+    method: "POST",
+    body: payload,
+  });
 };
 
 form.addEventListener("submit", handleSubmit);
