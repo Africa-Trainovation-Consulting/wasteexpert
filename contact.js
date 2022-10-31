7a0778d816b306a202e7b4fe875bccde5a56a283
gift56 <efeasiughu@gmail.com>
2022-10-31 12:29:40 +0100

    added some stuff

--- contact.js
+++ contact.js
@@ -3,13 +3,12 @@ const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}/;
 
 const handleSubmit = (e) => {
   e.preventDefault();
-
   let Url = "https://waste-expert-auth.herokuapp.com";
 
   const prePayload = new FormData(form);
 
   const payload = new URLSearchParams(prePayload);
-  
+
   console.log([...payload]);
   fetch(`${Url}/api/v1/auth/inquire`, {
     method: "POST",
@@ -20,6 +19,33 @@ const handleSubmit = (e) => {
       console.log(data);
     })
     .catch((err) => console.log(err.message));
+};
+
+const checkValidity = () => {
+  let [validName, validEmail, validSubject, validMsg] = [
+    false,
+    false,
+    false,
+    false,
+  ];
+
+  if (fullName.value === "") {
+    email.style.border = "1px solid red";
+  } else {
+    email.style.border = "1px solid green";
+    document.querySelector(".invalidfullName").innerHTML = "";
+    validName = true;
+  }
+  if (email.value === "") {
+    email.style.border = "1px solid red";
+  } else if (!pattern.test(email.value)) {
+    email.style.border = "1px solid red";
+    document.querySelector(".invalidEmail").innerHTML = "invalid email";
+  } else {
+    email.style.border = "1px solid green";
+    document.querySelector(".invalidEmail").innerHTML = "";
+    validEmail = true;
+  }
 };
 
 form.addEventListener("submit", handleSubmit);
