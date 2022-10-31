354deb37128dbd4e5103aa6272c50d98ba814ffb
gift56 <efeasiughu@gmail.com>
2022-10-31 12:35:39 +0100

    added some stuff

--- contact.js
+++ contact.js
@@ -1,26 +1,6 @@
 const form = document.getElementById("contactForm");
 const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}/;
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  let Url = "https://waste-expert-auth.herokuapp.com";
-
-  const prePayload = new FormData(form);
-
-  const payload = new URLSearchParams(prePayload);
-
-  console.log([...payload]);
-  fetch(`${Url}/api/v1/auth/inquire`, {
-    method: "POST",
-    body: payload,
-  })
-    .then((res) => res.json)
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((err) => console.log(err.message));
-};
-
 const checkValidity = () => {
   let [validName, validEmail, validSubject, validMsg] = [
     false,
@@ -68,5 +48,29 @@ function checkValidation() {
 
   checkValidity() ? window.location.assign(homePage) : " ";
 }
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  checkValidation();
+
+  
+  //   let Url = "https://waste-expert-auth.herokuapp.com";
+
+  //   const prePayload = new FormData(form);
+
+  //   const payload = new URLSearchParams(prePayload);
+
+  //   console.log([...payload]);
+  //   fetch(`${Url}/api/v1/auth/inquire`, {
+  //     method: "POST",
+  //     body: payload,
+  //   })
+  //     .then((res) => res.json)
+  //     .then((data) => {
+  //       console.log(data);
+  //     })
+  //     .catch((err) => console.log(err.message));
+};
 
 form.addEventListener("submit", handleSubmit);
